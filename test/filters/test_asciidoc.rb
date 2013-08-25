fbbdfa8acb4262b3f97dcb3cb8eba3cc06bb0150
Denis Defreyne <denis.defreyne@stoneship.org>
2013-08-25 17:07:03 +0200

    Use #run instead of removed #setup_and_run

--- test/filters/test_asciidoc.rb
+++ test/filters/test_asciidoc.rb
@@ -13,7 +13,7 @@ class Nanoc::AsciiDoc::FilterTest < Minitest::Test
     filter = ::Nanoc::AsciiDoc::Filter.new
 
     # Run filter
-    result = filter.setup_and_run("== Blah blah")
+    result = filter.run("== Blah blah")
     assert_match %r{<h2 id="_blah_blah">Blah blah</h2>}, result
   end
 
